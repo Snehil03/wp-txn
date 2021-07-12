e8d605a83e904904dc80bd7e75b1cea184c837e7
Snehil Belekar <snehil.belekar@borealisgroup.com>
2021-07-12 15:37:50 +0200

    updated repo

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 
 const app = {
     getBalanceByCategoryInPeriod: (transactions = [], category, start, end) => {
-        return (transactions.length === 0) ? 0 : transactions.reduce((balance, transaction) => {
-            return ((transaction.category === category)
-                && (new Date(transaction.time) >= start)
-                && (new Date(transaction.time) < end)) ? balance + transaction.amount : balance;
+        return (transactions.length === 0) ? 0 : transactions.reduce((balance, txn) => {
+            return ((txn.category === category)
+                && (new Date(txn.time) >= start)
+                && (new Date(txn.time) < end)) ? balance + txn.amount : balance;
         }, 0);
     }
 };
